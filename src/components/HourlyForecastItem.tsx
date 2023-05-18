7543d6070a62cf85d6702bc5cf98433d3ef68d85
DSI Bandung <willy.suyanto121212@gmail.com>
2023-05-19 06:41:02 +0700

    add hourly Forecast Item

--- src/components/HourlyForecastItem.tsx
+++ src/components/HourlyForecastItem.tsx
@@ -1,7 +1,12 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import {List} from '../models/ForecastData';
 import moment from 'moment';
+import {Image} from '@rneui/themed';
+import {getWeatherImageUrl, renderTemperature} from '../helpers/common';
+import {size} from '../theme/sizes';
+import {colors} from '../theme/colors';
+import {scale, scaleFont} from '../helpers/scale';
 
 interface HourlyForecastItemProps {
   item: null | List;
@@ -9,10 +14,50 @@ interface HourlyForecastItemProps {
 
 export default function HourlyForecastItem(props: HourlyForecastItemProps) {
   const item = props.item;
-  const renderTime = () => {};
+  const renderTime = (date: string | undefined) => {
+    if (date) {
+      const splitDate = date.split(' ');
+      if (splitDate[1] === '00:00:00') {
+        return moment(date).format('MMM DD');
+      }
+      return moment(date).format('HH:mm');
+    }
+  };
+  console.log(item);
   return (
-    <View>
-      <Text>{moment(item?.dt_txt).format('D')}</Text>
+    <View style={styles.forecastContainer}>
+      <Text style={styles.forecastDate}>{renderTime(item?.dt_txt)}</Text>
+      <Image
+        source={item ? {uri: getWeatherImageUrl(item.weather[0].icon)} : {}}
+        style={styles.forecastIcon}
+      />
+      <Text style={styles.forecastTemp}>
+        {renderTemperature(item?.main.temp)}
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  forecastIcon: {
+    height: 30,
+    width: 30,
+  },
+  forecastContainer: {
+    marginTop: size.md,
+    alignItems: 'center',
+    width: 60,
+    backgroundColor: colors.gray,
+    marginEnd: scale(10),
+    padding: scale(5),
+    borderRadius: 10,
+  },
+  forecastDate: {
+    color: colors.text,
+    fontWeight: '400',
+  },
+  forecastTemp: {
+    fontSize: scaleFont(16),
+    fontWeight: '600',
+  },
+});
--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,3 +1,10 @@
 export const getWeatherImageUrl = (code: string | undefined) => {
   return `https://openweathermap.org/img/wn/${code}@2x.png`;
 };
+
+export const renderTemperature = (temp: number | undefined) => {
+  if (temp) {
+    return `${Math.ceil(temp)}°C`;
+  }
+  return '';
+};
